c5f64300a7c3a52485a421f66661401b3aeddd33
Thomas <thomas.camlong@epitech.eu>
2020-02-14 16:23:50 +0100

    Version 0.1 done.

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# my_list
-Second flutter project, a startup name generator
+# Passport
+
+My **first** Flutter project.
+
+## What does it do ?
+Although in version 0.1, it aims to provide a simple and sleek app to show your personal card.
+### At the moment it does:
+* Displays the Name, UserName, Email and Phone number
+* Theme is dependent on the system theme (Dark / Light)
+* Display an image in a round box at the top
+* It has a personalised splashscreen
+
+*ATM the variables can only be changed in the code.*
+
+### **What i want to add:**
+* A settings button on the top right corner to set the theme
+* A "change image" button when clicking the image
+* A "new item" button on the bottom to add new items
+* Save the data to a file so it stays on reboot.
+
+Thank you for reading.
+
+If you are passing by and want to add suggestions you could open an issue :happy:
+
+also you could visit my website: [ajnart.xyz](https://ajnart.xyz)
---
+++ lib/main.dart
@@ -0,0 +1,130 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+import 'package:flutter/rendering.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      darkTheme: ThemeData.dark(),
+      themeMode: ThemeMode.dark,
+      theme: ThemeData(
+        primaryColor: Colors.green,
+        brightness: Brightness.light
+      ),
+      home: Scaffold(
+        body: Center(
+          child: RandomWords(),
+        ),
+      ),
+    );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  RandomWords({Key key}) : super(key: key);
+
+  @override
+  _RandomWordsState createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  final List<WordPair> _suggestions = <WordPair>[];
+  final Set<WordPair> _saved = Set<WordPair>();
+  final TextStyle _biggerFont = const TextStyle(fontSize: 18, fontWeight: FontWeight.bold);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('Startup Name Generator',
+            style: TextStyle(
+                fontWeight: FontWeight.bold,
+                letterSpacing: 3,
+                color: Colors.white,
+                wordSpacing: 2,
+                fontSize: 20)),
+        actions: <Widget>[
+          // Add 3 lines from here...
+          IconButton(icon: Icon(Icons.list), onPressed: _pushSaved),
+        ],
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16),
+        itemBuilder: (BuildContext _context, int i) {
+          if (i.isOdd) {
+            return Divider();
+          }
+
+          final int index = i ~/ 2;
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+          return _buildRow(_suggestions[index]);
+        });
+  }
+
+  Widget _buildRow(WordPair pair) {
+    final bool alreadySaved = _saved.contains(pair);
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+      trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: () {
+        // Add 9 lines from here...
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      }, 
+    );
+  }
+
+  void _pushSaved() {
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        builder: (BuildContext context) {
+          final Iterable<ListTile> tiles = _saved.map(
+            (WordPair pair) {
+              return ListTile(
+                title: Text(
+                  pair.asPascalCase,
+                  style: _biggerFont,
+                ),
+              );
+            },
+          );
+          final List<Widget> divided = ListTile.divideTiles(
+            context: context,
+            tiles: tiles,
+          ).toList();
+
+          return Scaffold(
+            // Add 6 lines from here...
+            appBar: AppBar(
+              title: Text('Saved Suggestions'),
+            ),
+            body: ListView(children: divided),
+          ); // ... to here.
+        },
+      ),
+    );
+  }
+}
